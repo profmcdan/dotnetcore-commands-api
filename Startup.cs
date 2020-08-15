11cfc2d6ba0b8272aa25ba9585232e8697861dd4
Daniel Ale <danielale9291@gmail.com>
2020-08-15 16:15:29 +0100

    Add Postgres database

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using Commander.Data;
 
 namespace Commander
@@ -26,7 +27,12 @@ namespace Commander
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommanderContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("CommanderConnection"));
+            });
             services.AddControllers();
+            services.AddScoped<ICommanderRepo, MockCommanderRepo>();
 
         }
 
